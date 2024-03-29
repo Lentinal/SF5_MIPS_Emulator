19ed334f9dfb430fab34f21bac822c5e713d7f6c
AJMonfort <ajmonfort02@gmail.com>
2024-03-28 22:53:40 -0400

    ...

--- MIPS_Emulator/Program.cs
+++ MIPS_Emulator/Program.cs
@@ -17,6 +17,7 @@ namespace MIPS_Emulator
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
