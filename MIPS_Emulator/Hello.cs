a222f1e727e834e110491d1bd1b7ee2f8b5617dd
crewsdel <124068095+crewsdel@users.noreply.github.com>
2024-03-28 22:54:10 -0400

    dd

--- MIPS_Emulator/Hello.cs
+++ MIPS_Emulator/Hello.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace MIPS_Emulator
 {
-    public partial class Form1 : Form
+    public partial class Hello : Form
     {
-        public Form1()
+        public Hello()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
